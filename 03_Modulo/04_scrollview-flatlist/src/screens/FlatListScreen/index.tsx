0f50b4831802a6b91d8ebcd647787e40faf0a215
athena272 <guilhermera272@gmail.com>
2024-08-25 00:19:58 -0300

    Update index.tsx

--- 03_Modulo/04_scrollview-flatlist/src/screens/FlatListScreen/index.tsx
+++ 03_Modulo/04_scrollview-flatlist/src/screens/FlatListScreen/index.tsx
@@ -1,5 +1,28 @@
 import { useNavigation } from "@react-navigation/native"
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+
+type Movie = {
+    id: number,
+    year: string,
+    title: string,
+}
+
+type MovieProp = {
+    item: Movie
+}
+
+const movies: Movie[] = [
+    { id: 1, year: "1994", title: "Um Sonho de Liberdade" },
+    { id: 2, year: "1972", title: "O Poderoso Chefão" },
+    { id: 3, year: "2008", title: "Batman: O Cavaleiro das Trevas" },
+    { id: 4, year: "1974", title: "O Poderoso Chefão II" },
+    { id: 5, year: "1957", title: "12 Homens e uma Sentença" },
+    { id: 6, year: "1993", title: "A Lista de Schindler" },
+    { id: 7, year: "2003", title: "O Senhor dos Anéis: O Retorno do Rei" },
+    { id: 8, year: "1994", title: "Pulp Fiction" },
+    { id: 9, year: "2001", title: "O Senhor dos Anéis: A Sociedade do Anel" },
+    { id: 10, year: "1966", title: "Três Homens em Conflito" },
+]
 
 export default function FlatListScreen() {
     const navigation = useNavigation()
@@ -7,6 +30,14 @@ export default function FlatListScreen() {
     const navigateBack = () => {
         navigation.goBack()
     }
+
+    const renderMovie = ({ item }: MovieProp) => (
+        <View style={styles.list}>
+            <Text>ID: {item.id}</Text>
+            <Text>Filme: {item.title}</Text>
+            <Text>Ano: {item.year}</Text>
+        </View>
+    )
 
     return (
         <View style={styles.container}>
@@ -18,6 +49,11 @@ export default function FlatListScreen() {
             >
                 <Text style={styles.buttonText}>Voltar</Text>
             </TouchableOpacity>
+            <FlatList
+                data={movies}
+                renderItem={renderMovie}
+                keyExtractor={(item) => item.id.toString()}
+            />
         </View>
     )
 }
@@ -42,4 +78,8 @@ const styles = StyleSheet.create({
     buttonText: {
         color: "#FFFFFF",
     },
+    list: {
+        marginTop: 40,
+        marginHorizontal: 10,
+    }
 })
