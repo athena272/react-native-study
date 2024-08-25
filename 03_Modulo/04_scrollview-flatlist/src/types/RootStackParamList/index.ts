96aff73c85293ccbe36c5548e50ade10af366e66
athena272 <guilhermera272@gmail.com>
2024-08-24 23:53:41 -0300

    Update index.ts

--- 03_Modulo/04_scrollview-flatlist/src/types/RootStackParamList/index.ts
+++ 03_Modulo/04_scrollview-flatlist/src/types/RootStackParamList/index.ts
@@ -1,6 +1,7 @@
 // Defina o tipo das rotas e seus parâmetros
 export type RootStackParamList = {
-    Home: undefined;
-    Navigation: undefined;
-    ScrollView: undefined;
+    Home: undefined
+    Navigation: undefined
+    ScrollView: undefined
+    FlatList: undefined
 };
