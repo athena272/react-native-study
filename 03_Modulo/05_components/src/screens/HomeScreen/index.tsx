ca2e60a77112213974df31d3c7257328a9a8635a
athena272 <guilhermera272@gmail.com>
2024-08-27 20:55:03 -0300

    Update index.tsx

--- 03_Modulo/05_components/src/screens/HomeScreen/index.tsx
+++ 03_Modulo/05_components/src/screens/HomeScreen/index.tsx
@@ -1,8 +1,9 @@
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { RootStackParamList } from '../../types/RootStackParamList';
 import NavButton from '../../components/NavButton';
+import Container from '../../components/Container';
+import Title from '../../components/Title';
 
 export default function HomeScreen() {
     // Tipar o useNavigation com o tipo correto
@@ -21,25 +22,12 @@ export default function HomeScreen() {
     }
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Olá, mundo!</Text>
+        <Container>
+            <Title text="Olá, mundo!" />
             <StatusBar style="auto" />
             <NavButton text='Aula de Navegação' onPress={navigateToNavigationScreen}/>
             <NavButton text='Aula de ScrollView' onPress={navigateToScrollViewScreen}/>
             <NavButton text='Aula de FlatList' onPress={navigateToFlatListScreen}/>
-        </View>
+        </Container>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 40,
-        color: '#f64348'
-    }
-});
