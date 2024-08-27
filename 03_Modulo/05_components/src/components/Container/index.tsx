90d244156f599f354195f1ae896e383733779e27
athena272 <guilhermera272@gmail.com>
2024-08-27 20:54:46 -0300

    Create index.tsx

---
+++ 03_Modulo/05_components/src/components/Container/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+type ContainerProps = {
+    children: React.ReactNode
+}
+
+export default function Container({ children }: ContainerProps) {
+    return (
+        <View style={styles.container}>
+            {children}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 16,
+        marginHorizontal: 3,
+        marginTop: 48
+    },
+})
