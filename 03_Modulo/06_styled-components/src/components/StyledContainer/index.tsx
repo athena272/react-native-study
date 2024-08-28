17b8cdccd4744402ed7b1bbccb75725f8e87bc7b
athena272 <guilhermera272@gmail.com>
2024-08-27 21:31:50 -0300

    Update index.tsx

--- 03_Modulo/06_styled-components/src/components/StyledContainer/index.tsx
+++ 03_Modulo/06_styled-components/src/components/StyledContainer/index.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components/native'
+import { ViewProps } from 'react-native';
 
-export default styled.View`
+interface StyledContainerProps extends ViewProps {
+    color?: string;
+}
+
+export default styled.View<StyledContainerProps>`
   flex: 1;
   margin: 32px 0 0;
   padding: 16px;
+  background-color: ${(props) => props.color ?? '#fff'};
 `
