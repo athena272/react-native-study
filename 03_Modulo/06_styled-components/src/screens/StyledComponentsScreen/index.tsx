2ab2ebc336d39e4a0395b2e37f4d0cf59efab61a
athena272 <guilhermera272@gmail.com>
2024-08-27 21:17:40 -0300

    Create index.tsx

---
+++ 03_Modulo/06_styled-components/src/screens/StyledComponentsScreen/index.tsx
@@ -0,0 +1,10 @@
+import StyledContainer from "../../components/StyledContainer";
+import StyledTitle from "../../components/StyledTitle";
+
+export default function StyledComponentsScreen() {
+    return (
+        <StyledContainer>
+            <StyledTitle>Styled Components</StyledTitle>
+        </StyledContainer>
+    )
+}
