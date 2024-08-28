b4dd6fea0decb9ec684bfd769e9242415bfe9517
athena272 <guilhermera272@gmail.com>
2024-08-27 21:17:47 -0300

    Update index.tsx

--- 03_Modulo/06_styled-components/src/components/StackNavigator/index.tsx
+++ 03_Modulo/06_styled-components/src/components/StackNavigator/index.tsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { RootStackParamList } from '../../types/RootStackParamList'
 import HomeScreen from "../../screens/HomeScreen"
 import NavigationScreen from "../../screens/NavigationScreen"
 import ScrollViewScreen from "../../screens/ScrollViewScreen"
 import FlatListScreen from "../../screens/FlatListScreen"
-import { RootStackParamList } from '../../types/RootStackParamList'
+import StyledComponentsScreen from "../../screens/StyledComponentsScreen"
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
@@ -21,6 +22,7 @@ export default function StackNavigator() {
                 <Stack.Screen name="Navigation" component={NavigationScreen} />
                 <Stack.Screen name="ScrollView" component={ScrollViewScreen} />
                 <Stack.Screen name="FlatList" component={FlatListScreen} />
+                <Stack.Screen name="Styled" component={StyledComponentsScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
